51dcb2161e36953752b7ebc12ef2353c80017a93
adam malantonio <malantoa@lafayette.edu>
2017-10-09 16:34:44 -0400

    add not-found handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+    def not_found(error)
+      render json: { error: error.message, status: 404 }, status: 404
+    end
 end
