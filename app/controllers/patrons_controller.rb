b1fd23d8db8321d9e803659d49d292336e301622
adam malantonio <malantoa@lafayette.edu>
2017-11-02 14:13:25 -0400

    update patrons controller to show individuals

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -3,6 +3,10 @@ class PatronsController < ApplicationController
     @patrons = Patron.paginate(page: params[:page], per_page: params[:per_page])
   end
 
+  def show
+    @patron = Patron.find(params[:id])
+  end
+
   def search
     @patrons = Patron.search(params[:q])
                      .paginate(page: params[:page], per_page: params[:per_page])
