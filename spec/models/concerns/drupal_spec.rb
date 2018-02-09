7e19bf38fbd176a64215a8316e2aa859753ef264
adam malantonio <malantoa@lafayette.edu>
2018-02-09 15:35:12 -0500

    finish coverage for drupal_spec.rb

--- spec/models/concerns/drupal_spec.rb
+++ spec/models/concerns/drupal_spec.rb
@@ -12,29 +12,42 @@ RSpec.describe Drupal do
     Object.send(:remove_const, :Drupalable)
   end
 
-  let(:drupal_node_id) { '1234' }
+  describe '.find_by_drupal_id' do
+    # using Subjects as our example factory
+    context 'when searching for a model with the Drupal mixin' do
+      let(:subjects) { create_list(:subject, 5) }
+      let(:target) { subjects.last }
 
-  context 'when drupal_node_type is "node"' do
-    subject do
-      Drupalable.new.tap do |d|
-        d.drupal_node_id = drupal_node_id
-        d.drupal_node_type = 'node'
+      it 'finds the target by its :drupal_node_id' do
+        expect(Subject.find_by_drupal_id(target.drupal_node_id)).to eq target
       end
     end
-
-    its(:drupal_url) { should include drupal_node_id }
-    its(:drupal_url) { should include 'node' }
   end
 
-  context 'when drupal_node_type is "taxonomy"' do
-    subject do
-      Drupalable.new.tap do |d|
-        d.drupal_node_id = drupal_node_id
-        d.drupal_node_type = 'taxonomy'
+  describe '#drupal_url' do
+    let(:drupal_node_id) { '1234' }
+    context 'when drupal_node_type is "node"' do
+      subject do
+        Drupalable.new.tap do |d|
+          d.drupal_node_id = drupal_node_id
+          d.drupal_node_type = 'node'
+        end
       end
+
+      its(:drupal_url) { should include drupal_node_id }
+      its(:drupal_url) { should include 'node' }
     end
 
-    its(:drupal_url) { should include drupal_node_id }
-    its(:drupal_url) { should include 'taxonomy/term' }
+    context 'when drupal_node_type is "taxonomy"' do
+      subject do
+        Drupalable.new.tap do |d|
+          d.drupal_node_id = drupal_node_id
+          d.drupal_node_type = 'taxonomy'
+        end
+      end
+
+      its(:drupal_url) { should include drupal_node_id }
+      its(:drupal_url) { should include 'taxonomy/term' }
+    end
   end
 end
