d591b064774fa9276b94e1fe53efddcaa06ac7e4
adam malantonio <malantoa@lafayette.edu>
2018-02-13 17:39:43 -0500

    spec out loans_controller

---
+++ spec/controllers/loans_controller_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe LoansController do
+  describe '#index' do
+    context 'its default behavior' do
+    end
+
+    context 'when :ledger_id passed' do
+    end
+
+    context 'when :work_id passed' do
+    end
+
+    context 'when :patron_id passed' do
+    end
+  end
+
+  describe '#show' do
+    context 'when :id == "random"' do
+    end
+  end
+end
