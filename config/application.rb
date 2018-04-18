069258589ed97edf6c114a98232bc1aa9ab62120
adam malantonio <malantoa@lafayette.edu>
2018-04-18 13:50:37 -0400

    revert rails-api changes

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 require_relative 'boot'
 
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_view/railtie'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,15 +10,6 @@ module ElCamino
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    config.api_only = true
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get]  
-      end
-    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,30 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  defaults format: :json do
-    root to: 'home#index'
+  root to: 'home#index'
 
-    resources :authors, :patrons, :subjects, :works, only: [:index, :show] do
-      collection do
-        get 'search'
-      end
+  resources :authors, :patrons, :subjects, :works, only: [:index, :show] do
+    collection do
+      get 'search'
     end
+  end
 
-    resources :authors, :subjects, only: [:show] do
-      resources :works, only: [:index]
-    end
+  resources :authors, :subjects, only: [:show] do
+    resources :works, only: [:index]
+  end
 
+  resources :loans, only: [:index, :show]
+
+  resources :ledgers, only: [:index, :show] do
     resources :loans, only: [:index, :show]
-    
-    resources :ledgers, only: [:index, :show] do
-      resources :loans, only: [:index, :show]
-    end
+  end
 
-    resources :patrons, only: [:index, :show] do
-      resources :loans, only: [:index, :show]
-    end
+  resources :patrons, only: [:index, :show] do
+    resources :loans, only: [:index, :show]
+  end
 
-    resources :works, only: [:index, :show] do
-      resources :loans, only: [:index]
-      resources :items, only: [:index]
-    end
+  resources :works, only: [:index, :show] do
+    resources :loans, only: [:index]
+    resources :items, only: [:index]
   end
 end
